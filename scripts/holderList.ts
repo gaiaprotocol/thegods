d80ab97f5026aeecd8d5e8f903276bd76e26a9cc
Sim Young Jae <hanul@hanul.me>
2023-02-24 21:59:29 +0900

    Update holderList.ts

--- scripts/holderList.ts
+++ scripts/holderList.ts
@@ -1,13 +1,12 @@
 import newHolders from "./new-holders.json";
 
-// TODO: Replace treasury address
-export const treasury = "0x1230000000000000000000000000000000000321";
+export const treasury = "0xe7b16698ffe31B453F544Ee57063f60e4F94C8d8";
 
 export const holders = Object.keys(newHolders);
 
 export const amounts: number[] = [];
 for (const holder of holders) {
-    amounts.push((newHolders as any)[holder].length);
+    amounts.push((newHolders as any)[holder].length - (holder === "0x8033cEB86c71EbBF575fF7015FcB8F1689d90aC1" ? 1 : 0));
 }
 
 holders.push(treasury);
